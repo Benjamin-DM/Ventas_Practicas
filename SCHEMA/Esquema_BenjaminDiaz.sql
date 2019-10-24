37ba924983600b0418ef1e9cf7e7f13443b94862
Benjamin-DM <benjamin.diaz@vallegrande.edu.pe>
2019-10-24 18:54:30 -0500

    practica db

--- SCHEMA/Esquema_BenjaminDiaz.sql
+++ SCHEMA/Esquema_BenjaminDiaz.sql
@@ -1,7 +1,7 @@
-CREATE DATABASE IF NOT EXISTS VENTAS ;
+CREATE DATABASE IF NOT EXISTS VENTAS;
 USE VENTAS;
-CREATE OR REPLACE TABLE CUSTOMER (
-CUSTNO char (8) COMMENT 'Aqui va el numero de customer',
+CREATE TABLE CUSTOMER (
+CUSTCOD char (8) COMMENT 'Aqui va el numero de customer',
 CUSTFIRSTNAME varchar (80) NOT NULL COMMENT 'Aqui va el primer nombre',
 CUSTLASTNAME varchar(80) NOT NULL COMMENT 'Aqui va el apellido',
 CUSTCITY varchar (80) NOT NULL COMMENT 'Aqui va la ciudad',
@@ -11,21 +11,19 @@ CUSTBAL decimal (10,2) NOT NULL COMMENT 'Aqui va el dinero de la venta',
  CONSTRAINT CUSTOMER PRIMARY KEY (CUSTNO)
 )COMMENT 'Esta tabla tiene los datos principales de la persona';
 
-CREATE OR REPLACE TABLE EMPLOYEE (
-EMPNO char (8) NOT NULL COMMENT 'Aqui va el ID DEL EMPLEADO',
+CREATE TABLE EMPLOYEE (
+EMPCOD char (8) NOT NULL COMMENT 'Aqui va el ID DEL EMPLEADO',
 EMPFIRSTNAME varchar (80) NOT NULL COMMENT 'Aqui va el primer nombre',
 EMPLASTNAME varchar(80) NOT NULL COMMENT 'Aqui va el apellido',
 EMPPHONE varchar (80) NOT NULL COMMENT 'Aqui va EL CELULAR DEL EMPLEADO',
 EMPEMAIL varchar (180) NOT NULL COMMENT 'Aqui va el EMAIL DEL EMPLEADO',
-
  CONSTRAINT EMPLOYEE PRIMARY KEY (EMPNO)
 )COMMENT 'Esta tabla tiene los datos principales de lOS EMPLEADOS';
 
-CREATE OR REPLACE TABLE ORDERTBL (
-ORDNO char (8) NOT NULL COMMENT 'Aqui va el ID DE LA TABLA',
+CREATE TABLE ORDERTBL (
+ORDCOD char (8) NOT NULL COMMENT 'Aqui va el ID DE LA TABLA',
 ORDDATE date NOT NULL COMMENT 'Aqui va la fecha',
-CUSTNO char (8) NOT NULL COMMENT 'id de la tabla customer',
-EMPNO char (8) COMMENT 'Aqui va EL id de la tabla empleado',
-
+CUSTCOD char (8) NOT NULL COMMENT 'id de la tabla customer',
+EMPCOD char (8) COMMENT 'Aqui va EL id de la tabla empleado',
  CONSTRAINT ORDERTBL PRIMARY KEY (ORDNO)
 )COMMENT 'Esta tabla ES PARA ORDENAR LOS DATOS';
